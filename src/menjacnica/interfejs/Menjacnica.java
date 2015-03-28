a80979c4f5ff67840e7dd714f0eb5e8b920ff27d
dusicastepic <dusica.stepic@gmail.com>
2015-03-29 00:41:30 +0100

    Napravljena Klasa Menjacnica koja implementira interfejs MenjacnicaIneterfejs

---
+++ src/menjacnica/interfejs/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica.interfejs;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.KursValute;
+import menjacnica.Valuta;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodaj(Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void izbrisi(Valuta valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public KursValute vratiKurs(GregorianCalendar datum, String ime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
