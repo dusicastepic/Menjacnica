fe2951994cb676d3920c6efa8467d1f87ad9cd9a
dusicastepic <dusica.stepic@gmail.com>
2015-03-29 00:53:28 +0100

    Implementirana je klasa Menjacnica koja implementira interfejs

--- src/menjacnica/interfejs/Menjacnica.java
+++ src/menjacnica/interfejs/Menjacnica.java
@@ -1,24 +1,37 @@
 package menjacnica.interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.KursValute;
 import menjacnica.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodaj(Valuta valuta) {
-		// TODO Auto-generated method stub
+		if(valuta==null)
+			throw new RuntimeException("Valuta ne moze da bude null! ");
+		if(valute.contains(valuta)) 
+			throw new RuntimeException("Valuta vec postoji ");
+		valute.add(valuta);
 
 	}
 
 	public void izbrisi(Valuta valuta) {
-		// TODO Auto-generated method stub
-
+		if(valuta==null) 
+			throw new RuntimeException("Valuta ne moze da bude null! ");
+		if(!valute.contains(valuta)) 
+			throw new RuntimeException("Valuta ne postoji");
+		valute.remove(valuta);
 	}
 
 	public KursValute vratiKurs(GregorianCalendar datum, String ime) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getIme().equals(ime)&&valute.get(i).getKv().getDatum().get(GregorianCalendar.DAY_OF_MONTH)==datum.get(GregorianCalendar.DAY_OF_MONTH))
+				return valute.get(i).getKv();
+			}
 		return null;
 	}
 
