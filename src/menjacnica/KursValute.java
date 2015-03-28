28a0e0d35aefe230a1bdb627b166c458c0e3e32d
dusicastepic <dusica.stepic@gmail.com>
2015-03-28 23:59:33 +0100

    Napravljene klase KursValute, Valuta atributi, get i set metode

---
+++ src/menjacnica/KursValute.java
@@ -0,0 +1,36 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class KursValute {
+
+	private GregorianCalendar datum;
+	private double prodajniKurs, srednjiKurs, kupovniKurs;
+	
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+
+
+}
---
+++ src/menjacnica/Valuta.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+public class Valuta {
+	
+	private String ime, skracenica;
+	KursValute kv;
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getSkracenica() {
+		return skracenica;
+	}
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+	public KursValute getKv() {
+		return kv;
+	}
+	public void setKv(KursValute kv) {
+		this.kv = kv;
+	}
+	
+	
+
+}
